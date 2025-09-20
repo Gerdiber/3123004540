19ad5b15f6be629da7941a37c3818ebc44e9759c
Gerdiber <m13544959762@163.com>
2025-09-20 21:06:16 +0800

    实现词频统计生成向量

--- main.py
+++ main.py
@@ -15,4 +15,26 @@ def filterAndCut(content):
     cutText = jieba.lcut(filterdText)
     return cutText
 
+#获取原文与修改文的词频向量
+def getVector(origCText,copyCText):
+    #总词汇
+    allword = set(origCText) | set(copyCText)
+    #原文词频向量vec1
+    vec1 = [0] * len(allword)
+    #修改文词频向量vec2
+    vec2 = [0] * len(allword)
+    #分词与下标配对
+    word_index={word:i for i,word in enumerate(allword)}
+    #计算两分词的各词出现次数
+    for word in origCText:
+        index=word_index.get(word)
+        if index is not None:
+            vec1[index]+=1
+    for word in copyCText:
+        index = word_index.get(word)
+        if index is not None:
+            vec2[index] += 1
+    return vec1,vec2
+
+
 print(filterAndCut(readTXT("orig.txt")))
