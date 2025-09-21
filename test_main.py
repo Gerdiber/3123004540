5108a8588cd3dc14ec16d01c663fe681e9629b98
Gerdiber <m13544959762@163.com>
2025-09-21 15:33:11 +0800

    给twst_main.py添加点注释

--- test_main.py
+++ test_main.py
@@ -3,9 +3,9 @@ import os
 from unittest.mock import patch
 from io import StringIO
 from main import readTXT,filterAndCut,getVector,cosineSimilarity,compute_similarity,main
-
+#测试各模块函数功能
 class TestCheck(unittest.TestCase):
-
+    #文件读取模块
     def test1_readTXT(self):
         with open('only_for_temp_test.txt','w') as file:
             file.write('3123004540')
@@ -16,12 +16,12 @@ class TestCheck(unittest.TestCase):
         with self.assertRaises(SystemExit) as cm:
             readTXT('不会存在的路径/大顶堆/大多数','测试文件')
         self.assertEqual(cm.exception.code,'InvalidPath!')
-
+    #文本处理模块
     def test_filterAndCut(self):
         text='今天是星期天,天气晴,今天晚上我要去看电影.triangle double test007.'
         expected=['今天','是','星期天','天气','晴','今天','晚上','我要','去','看','电影','triangle','double','test007']
         self.assertEquals(filterAndCut(text),expected)
-
+    #向量生成模块
     def test_getvector(self):
         word1=['我','你','它','更好','我','你','更好','更好','你','我','你']
         word2=['你','更好']
@@ -30,7 +30,7 @@ class TestCheck(unittest.TestCase):
         for i in range(len(vec1)):
             sum+=vec1[i]*vec2[i]
         self.assertEquals(sum,7)
-
+    #余弦相似度计算模块
     def test1_cosineSimilarity(self):
         vec1=[1,2,3]
         vec2=[1,2,3]
@@ -43,7 +43,7 @@ class TestCheck(unittest.TestCase):
         vec1=[0,0,0]
         vec2=[0,0,0]
         self.assertEquals(cosineSimilarity(vec1,vec2),1.0)
-
+    #总体计算模块
     def test_compute_similarity(self):
         origText='今天是星期天,天气晴,今天晚上我要去看电影.triangle double test007.'
         copyText='今天是周天,天气晴朗,我晚上要去看电影.trishape float test005.'
@@ -51,7 +51,7 @@ class TestCheck(unittest.TestCase):
         self.assertIsInstance(similarity,float)
         self.assertGreaterEqual(similarity,0.0)
         self.assertLessEqual(similarity,1.0)
-
+#测试主函数
 class TestMain(unittest.TestCase):
     @patch('sys.argv',['main.py','orig.txt','orig_copy.txt','ans.txt'])
     @patch('main.readTXT')
